8d386a59654cdcedc13e9804c0d174d7701c833f
pradipmiskin <125717350+pradipmiskin@users.noreply.github.com>
2023-03-30 11:43:50 +0530

    Add files via upload

---
+++ forloopsum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class forloopsum
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a number");
+int a=sc.nextInt();
+int ca=0;
+for(int i=1;i>0;i=1/10)
+{
+int ram=a%10;
+ca++;
+}
+System.out.println(ca);
+}
+}
